bdecc049848dd7a3a97c474ff26e35d7a8e92b96
khamdullaevuz <elbek.khamdullaev@gmail.com>
2024-06-13 22:19:18 +0500

    fix

--- router/main.go
+++ router/main.go
@@ -11,21 +11,24 @@ import (
 func Init() *gin.Engine {
 	router := gin.Default()
 
-	router.Use(middlewares.JsonMiddleware())
+	v1 := router.Group("/api/v1")
+	{
+		v1.Use(middlewares.JsonMiddleware())
+		v1.GET("/services", func(c *gin.Context) {
+			service := services.NewMethodService()
 
-	router.GET("/services", func(c *gin.Context) {
-		
-		service := services.NewMethodService()
+			response, error := service.SwitchMethods(
+				c.MustGet("request").(models.Request),
+			)
 
-		response, error := service.SwitchMethods(c.MustGet("request").(models.Request))
+			if error != nil{
+				c.JSON(400, error)
+			}else{
+				c.JSON(200, response)
+			}
 
-		if error != nil{
-			c.JSON(400, error)
-		}else{
-			c.JSON(200, response)
-		}
-
-	})
+		})
+	}
 
 	return router
 }
